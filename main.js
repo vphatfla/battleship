daf6243c5393c3c8a551b85748ce3ba6d6bd52a3
vphatfla <pvphat25@gmail.com>
2021-10-29 20:58:43 -0400

    dont ship factory and gameboard function for placeship correctly

---
+++ main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var a =3;
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vYmF0dGxlc2hpcC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJ2YXIgYSA9MztcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
